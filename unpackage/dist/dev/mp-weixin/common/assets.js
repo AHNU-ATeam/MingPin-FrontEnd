c420ba61b5f8fbd7c20dcdb1829eed774941cbd5
husir111 <1848722306@qq.com>
2024-08-06 13:34:39 +0800

    firstpush

---
+++ unpackage/dist/dev/mp-weixin/common/assets.js
@@ -0,0 +1,7 @@
+"use strict";
+const _imports_0 = "/static/swiper/iamge1.jpeg";
+const _imports_1 = "/static/swiper/image2.jpeg";
+const _imports_2 = "/static/swiper/image3.jpeg";
+exports._imports_0 = _imports_0;
+exports._imports_1 = _imports_1;
+exports._imports_2 = _imports_2;
